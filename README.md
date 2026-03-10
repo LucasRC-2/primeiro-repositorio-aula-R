d605b07ded2fc5e75556104cd676c2f1e85fe56b
Lucas Ribas Casanova <lrc.biot@gmail.com>
2026-03-10 10:39:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # primeiro-repositorio-aula-R
 Aula 2 Introdução ao R Diogo Provete
 # aula 2 teste
+#Lucas teste 2
 
---
+++ primeiro-repositorio-aula-R1.R
@@ -0,0 +1,28 @@
+# Tue Mar 10 10:15:52 2026 ------------------------------
+
+librabry(usethis)
+{usethis}
+usethis::git_sitrep()
+usethis::use_git_config(
+  user.name = "Lucas Ribas Casanova",
+  user.email = "lrc.biot@gmail.com"
+)
+usethis::git_sitrep()
+
+# atividade 5.7.3
+create_github_token()
+
+# Atividade 5.7.4
+gitcreds::gitcreds_set()
+
+# Session -> restart R para salver o Token no R e nao precisar ficar salvando
+
+# Atividade 5.7.5
+# Confirmar se esta conectado ao GitHub, personal acces token deve estar <discovered>
+usethis::git_sitrep()
+
+# Atividade 5.8.6
+usethis::use_git()
+
+usethis::use_github()
+2
